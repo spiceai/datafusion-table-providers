cf87f900280a9c691ca1a343c96c3b568301ea2b
peasee <98815791+peasee@users.noreply.github.com>
2026-07-02 11:53:55 +1000

    fix: Remove maybe_source_err generic

--- core/src/sql/db_connection_pool/mod.rs
+++ core/src/sql/db_connection_pool/mod.rs
@@ -112,7 +112,3 @@ impl DbInstanceKey {
         DbInstanceKey::File(path)
     }
 }
-
-pub(crate) fn maybe_source_err<T: std::error::Error>(err: &T) -> &dyn std::error::Error {
-    err.source().unwrap_or(err)
-}
